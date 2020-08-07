6cb037847b1620e45d10b7ef9a764ffc6d6a12fb
stefanfeldner@gmail.com <stefanfeldner@gmail.com>
2020-08-07 19:48:27 +0200

    Changing port

--- src/server/server.js
+++ src/server/server.js
@@ -26,7 +26,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // designates what port the app will listen to for incoming requests
-app.listen(process.env.PORT || 80, function () {
+app.listen(process.env.PORT || 8081, function () {
     console.log('Example app listening on port 8081!');
 });
 
